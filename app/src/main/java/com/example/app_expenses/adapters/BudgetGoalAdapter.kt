f0807c2128a5df3b953bbd0f2a3b2a14382d8a1d
jmedeiros11 <jmedeiros1011@gmail.com>
2022-12-29 16:07:19 -0500

    Fixed UI issues when budget was 0 in home page

--- app/src/main/java/com/example/app_expenses/adapters/BudgetGoalAdapter.kt
+++ app/src/main/java/com/example/app_expenses/adapters/BudgetGoalAdapter.kt
@@ -47,11 +47,15 @@ class BudgetGoalAdapter(): RecyclerView.Adapter<BudgetGoalAdapter.ViewHolder>() 
         holder.itemPercentage.text = "${percentage.toInt()}%"
         holder.itemProgressBar.setProgressCompat(percentage.toInt(), true)
 
-        if(percentage.toInt() <= 100){
+
+        if(totalSpent <= totalCategoryBudget){
             holder.itemProgressBar.setIndicatorColor(ContextCompat.getColor(context, category?.categoryColor!!))
             holder.itemTotalBudget.setTextColor(ContextCompat.getColor(context, category?.categoryColor!!))
             holder.itemPercentage.setTextColor(ContextCompat.getColor(context, R.color.white))
         } else{
+            if(totalCategoryBudget == 0f){
+                holder.itemPercentage.text = "N/A"
+            }
             holder.itemProgressBar.setIndicatorColor(ContextCompat.getColor(context, R.color.red_bright))
             holder.itemTotalBudget.setTextColor(ContextCompat.getColor(context, R.color.red_bright))
             holder.itemPercentage.setTextColor(ContextCompat.getColor(context, R.color.red_bright))
